3578280f342ab2a44ad67e5e08c62818aaebda57
Li <jason.li@fiserv.com>
2023-10-16 15:44:35 -0400

    fizzBuzz done

---
+++ FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz {
+
+
+    static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            }
+            else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            }
+            else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            }
+            else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
